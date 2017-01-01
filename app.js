b4b8ed9e2c34cf760d753cfa62f8fa6d3028c01e
Apr7174 <apr.7174@gmail.com>
2017-01-01 18:30:38 -0500

    added calcualtion functionality

--- app.js
+++ app.js
@@ -5,4 +5,32 @@ angular.module('myApp', [])
 		// };
 	.run(function() {
 		console.log("does it run");
-	});	
+	})	
+
+	.controller('MyController', function($scope){
+		$scope.enterMeal = function(){
+			$scope.subtotal = $scope.mealBase + ($scope.mealBase * $scope.taxRate / 100);
+			$scope.tip = ($scope.subtotal * $scope.tipPercentage / 100);
+			$scope.total = $scope.subtotal + $scope.tip;
+
+			$scope.tipTotal += $scope.tip;
+			$scope.mealCount ++;
+			$scope.averageTip = $scope.tipTotal / $scope.mealCount;			
+		}
+
+		$scope.resetMeal = function(){
+			$scope.mealBase = 0;
+			$scope.taxRate = 0;
+			$scope.tipPercentage = 0;
+		}
+
+		$scope.resetEarnings = function(){
+			$scope.tipTotal = 0;
+			$scope.mealCount = 0;	
+			$scope.averageTip = 0;
+		}
+
+		//initalizes values
+		$scope.resetEarnings();
+		$scope.resetMeal();
+	});
